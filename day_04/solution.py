d7dcf501dee4aad9a1e175618d12afeaf39e5f3b
kermitnirmit <kermitnirmit@gmail.com>
2024-12-04 00:24:47 -0500

    day 4

---
+++ day_04/solution.py
@@ -0,0 +1,67 @@
+from collections import deque, Counter
+
+f = [x.strip() for x in open("input.txt").readlines()]
+neighbors_2d = [(x, y) for y in range(-1, 2) for x in range(-1, 2) if (x, y) != (0, 0)]
+diag_neighbors = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+a = 0
+starts = []
+for i in range(len(f)):
+    for j in range(len(f[i])):
+        if f[i][j] == "X":
+            starts.append((i, j, (0, 0), 0))
+target = "XMAS"
+q = deque(starts)
+
+while q:
+    ci, cj, d, i = q.popleft()
+    if i == 3 and f[ci][cj] == "S":
+        a += 1
+    else:
+        if d == (0, 0):
+            for di, dj in neighbors_2d:
+                ni, nj = ci + di, cj + dj
+                if 0 <= ni < len(f) and 0 <= nj < len(f[ni]) and f[ni][nj] == "M":
+                    q.append((ni, nj, (di, dj), 1))
+        else:
+            di, dj = d
+            ni, nj = ci + di, cj + dj
+            if 0 <= ni < len(f) and 0 <= nj < len(f[ni]) and f[ni][nj] == target[i + 1]:
+                q.append((ni, nj, (di, dj), i + 1))
+print(a)
+
+p2 = 0
+
+starts = []
+for i in range(len(f)):
+    for j in range(len(f[i])):
+        if f[i][j] == "A":
+            starts.append((i, j))
+
+q = deque(starts)
+
+
+def valid_point(i, j):
+    return 0 <= i < len(f) and 0 <= j < len(f[i])
+
+
+while q:
+    ci, cj = q.popleft()
+    n_vals = []
+    for di, dj in diag_neighbors:
+        ni, nj = ci + di, cj + dj
+        if valid_point(ni, nj):
+            n_vals.append(f[ni][nj])
+    valid = False
+    if "X" in n_vals:
+        continue
+    if len(n_vals) == 4:
+        c = Counter(n_vals)
+        if c["M"] != 2 or c["S"] != 2:
+            continue
+        for i in range(len(n_vals)):
+            if n_vals[i] != n_vals[i - 2] and n_vals[i] in "MS" and n_vals[i - 2] in "MS":
+                valid = True
+            else:
+                valid = False
+    p2 += valid
+print(p2)
